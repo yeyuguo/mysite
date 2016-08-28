fd0fa413b62a7cb84e4d1d7ca07ec3577eb370e0
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 16:31:11 +0800

    add TestCase code(error result):   polls/tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,7 @@ from django.test import TestCase
 
 from .models import Question
 
-
+# python manage test polls
 class QuestionMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
     	time = timezone.now() + datetime.timedelta(days=30)
