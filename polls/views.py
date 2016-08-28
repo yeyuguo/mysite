f05efca53cda8c70caeed49736ab1949e6057046
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 15:42:37 +0800

    vote post form:   db.sqlite3 	vote post form:   polls/templates/polls/detail.html         vote post form:   polls/views.py 	vote post form:polls/templates/polls/results.html

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404 # for detail
+from django.http import HttpResponseRedirect, HttpResponse # for vote
+from django.core.urlresolvers import reverse
 
 from models import Choice,Question
 from django.shortcuts import get_object_or_404,render
@@ -19,27 +21,28 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-    # p = get_object_or_404(Question, pk=question_id)
-    # try:
-    #     selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    # except (KeyError, Choice.DoesNotExist):
-    #     # Redisplay the question voting form.
-    #     return render(request, 'polls/detail.html', {
-    #         'question': p,
-    #         'error_message': "You didn't select a choice.",
-    #     })
-    # else:
-    #     selected_choice.votes += 1
-    #     selected_choice.save()
-    #     # Always return an HttpResponseRedirect after successfully dealing
-    #     # with POST data. This prevents data from being posted twice if a
-    #     # user hits the Back button.
-    #     return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        
 
 from .models import Question
 def polls_temp(request):
