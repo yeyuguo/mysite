0bca32b84119d20f0284d647a093fa21844651e2
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 16:00:27 +0800

    reconsitution the url route

--- polls/views.py
+++ polls/views.py
@@ -8,22 +8,26 @@ from django.core.urlresolvers import reverse
 from models import Choice,Question
 from django.shortcuts import get_object_or_404,render
 
-def index(request):
-	return HttpResponse('hello,you\'re at the polls index html.')
+#reconsitution url route
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
@@ -44,7 +48,6 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
         
 
-from .models import Question
 def polls_temp(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([p.question_text for p in latest_question_list])
