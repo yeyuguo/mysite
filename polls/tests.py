dee6441331e5f13c4099dd15e9a288ab1253177b
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 16:30:09 +0800

    add TestCase code(error result):   polls/tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+    	time = timezone.now() + datetime.timedelta(days=30)
+    	future_question = Question(pub_date = time)
+    	self.asserEqual(future_question.test_was_published_recently_with_future_question(),False)
