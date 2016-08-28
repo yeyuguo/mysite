4748badf65dc0b56f3425358cbb9d3721a2e2944
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 18:45:29 +0800

    complete TestCase in study page:   polls/tests.py complete TestCase in study page:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -11,18 +11,27 @@ from django.shortcuts import get_object_or_404,render
 #reconsitution url route
 from django.views import generic
 
+# TestCase bug fix
+from django.utils import timezone
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        # TestCase  bug fixed 
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    # TestCase  bug fixed 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
