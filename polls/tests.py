91bbc65fdecc99744a6cca3880cb5fedbac2cf49
yeyuguo <yeyuguo@bj-wind.com>
2016-08-28 18:37:12 +0800

    add TestCase:   polls/models.py 	add TestCase:   polls/tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,3 @@
-from django.test import TestCase
-
-# Create your tests here.
 import datetime
 
 from django.utils import timezone
@@ -8,9 +5,23 @@ from django.test import TestCase
 
 from .models import Question
 
-# python manage test polls
+
 class QuestionMethodTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-    	time = timezone.now() + datetime.timedelta(days=30)
-    	future_question = Question(pub_date = time)
-    	self.asserEqual(future_question.test_was_published_recently_with_future_question(),False)
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for questions whose
+		pub_date is in the future.
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+	    """
+	    was_published_recently() should return True for questions whose
+	    pub_date is within the last day.
+	    """
+	    time = timezone.now() - datetime.timedelta(hours=1)
+	    recent_question = Question(pub_date=time)
+	    self.assertEqual(recent_question.was_published_recently(), True)
